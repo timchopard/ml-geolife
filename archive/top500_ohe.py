9862a927ed96aa07a7fa3b3119c53e9085d06986
Darren Rawlings <darren.rawlings@gmail.com>
2024-04-10 13:22:12 +0200

    Add one hot encoder for top500

---
+++ archive/top500_ohe.py
@@ -0,0 +1,28 @@
+
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+
+top500_df = pd.read_csv("../processed_data/partially_processed/top500.csv")
+
+top500_df = top500_df.drop(columns=['geoUncertaintyInM', 'country'])
+
+with pd.option_context('use_inf_as_na', True):
+    top500_df.areaInM2 = top500_df.areaInM2.fillna(top500_df.areaInM2.mean())
+
+top500_df.speciesId = top500_df.speciesId.astype('uint16')
+
+ohe = OneHotEncoder(dtype='bool')
+
+transformed = ohe.fit_transform(top500_df[['region', 'speciesId']])
+
+top500_df[np.concatenate((ohe.categories_[0], ohe.categories_[1]), axis=0)] = transformed.toarray()
+
+top500_df_grouped = top500_df.groupby('surveyId').max().copy()
+
+print(top500_df_grouped.shape)
+
+top500_df_grouped.to_pickle("../processed_data/partially_processed/top500_df_elevation_onehot_grouped.pkl")
+
+
