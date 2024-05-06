7e9d0ca9813b3565158eb3df62780ee3d1552892
tim <timothyjchandler@gmail.com>
2024-05-06 18:36:59 +0200

    added helper function to get species from output

--- helper/__init__.py
+++ helper/__init__.py
@@ -4,3 +4,4 @@ from .verifytest import verify_test
 from .verifysubmission import verify_submission
 from .images_to_np import load_images
 from .tifnormalizer import tif_helper
+from .top_n_regression import get_top_n
