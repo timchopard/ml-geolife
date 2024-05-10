048e02289b8181f68d85a8c86f95d2ecf58db8af
Darren Rawlings <darren.rawlings@gmail.com>
2024-05-10 08:29:20 +0200

    Add rfc files

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@
 """
 import sys
 from pipeline.getdata import DirectoryManagement
-from pipeline.preprocess import pickle_landsat
+
+# from pipeline.preprocess import pickle_landsat
 from pipeline.preprocess import PCACreator
 from pipeline.preprocess import DataCollector
 
@@ -11,10 +12,11 @@ if __name__ == "__main__":
     args = sys.argv
     if "-h" in args or len(args) == 1:
         print(
-            ":: Flags:\n\t-d  :  Download the data from kaggle (requires "     \
-            + "kaggle CLI)\n\t-p  :  Preprocess and pickle the data as single "\
-            + "train and test files\n\t-r  :  Reduce the dimensions of the "   \
-            + "data using PCA, saving the PCA models for later use")
+            ":: Flags:\n\t-d  :  Download the data from kaggle (requires "
+            + "kaggle CLI)\n\t-p  :  Preprocess and pickle the data as single "
+            + "train and test files\n\t-r  :  Reduce the dimensions of the "
+            + "data using PCA, saving the PCA models for later use"
+        )
     if "-d" in args:
         DirectoryManagement()
     if "-p" in args:
