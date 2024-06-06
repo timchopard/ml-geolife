36ce062503655ff0df48579396dc8e0c3a4d4cb9
tim <timothyjchandler@gmail.com>
2024-06-06 15:32:08 +0200

    added preprocess file

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,8 +1,12 @@
 import pickle as pk
 from pipeline2 import GeoLifePreprocessor, GeoLifePCA
+from pipeline2.preprocessing import pickle_landsat
 
 if __name__ == "__main__":
     train, test = GeoLifePreprocessor().process_data()
     train, test = GeoLifePCA(train, test).process_data()
     pk.dump(train, open("data/processed/train.pkl", "wb"))
     pk.dump(test, open("data/processed/test.pkl", "wb"))
+    train_ls, test_ls = pickle_landsat()
+    pk.dump(train_ls, open("data/processed/train_ls", "wb"))
+    pk.dump(test_ls, open("data/processed/test_ls", "wb"))
